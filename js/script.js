98272062ec2f4c9f66f1647671ccdbe5d210533b
DingerM GD2-2 <dinger.matrix@gmail.com>
2023-05-05 00:06:58 +0200

    Rajout des desc de  projets

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//#region Buttons
 $(document).ready(function(){
   $(".top-bar-right-buttons").mouseenter(function(){
       var fourChildNode = this.querySelector('.nav-bar');
@@ -9,17 +10,17 @@ $(document).ready(function(){
   });
 
   $(".top-bar-right-buttons").mousedown(function(){
-      this.style.color = "#FFD900";
+      this.fourChildNode.style.color = "#FFD900";
   });
 
   $(".top-bar-right-buttons").mouseup(function(){
       this.style.backgroundColor = this.parentElement.style.backgroundColor;
   });
 });
+//#endregion
 
-var _hasBeenInShadow = false;
+//#region Block scroll
 
-var _isTop = false;
 
 var _canScroll = true;
 
@@ -55,9 +56,23 @@ function PreventScrollForScrollKeys(e){
   }
 }
 
+
+function TimerScrollDisable(){
+  if(_canScroll == true) return;
+  _canScroll=true;
+  _notTimeout = null;
+}
+
+//#endregion
+
+//#region Effect top of page
+var _hasBeenInShadow = false;
+
+var _isTop = false;
+
 $( document ).ready(function() {
   HasScrollFromTheTop();
-  });
+});
   
 window.onscroll = function() {HasScrollFromTheTop()};
 
@@ -74,7 +89,6 @@ function HasScrollFromTheTop() {
   if (document.documentElement.scrollTop != 0 && _isTop) {
     _isTop = false;
     window.scrollTo(0, Position(document.getElementById("anchor-about-me")));
-    console.log("I stop scrolling");
     _canScroll = false;
     if(_notTimeout != null)
       clearTimeout(_notTimeout);
@@ -86,12 +100,6 @@ function HasScrollFromTheTop() {
   }
 }
 
-function TimerScrollDisable(){
-  if(_canScroll == true) return;
-  console.log("I can scroll");
-  _canScroll=true;
-  _notTimeout = null;
-}
 
 function Position(obj){
   var currenttop = 0;
@@ -101,4 +109,61 @@ function Position(obj){
    }while ((obj = obj.offsetParent));
    return [currenttop];
    }
- }
+}
+
+//#endregion
+
+const collectionN1 = document.getElementsByClassName("desc-project n1");
+
+const collectionN2 = document.getElementsByClassName("desc-project n2");
+
+var _n1Active = false;
+
+var _n2Active = false;
+
+$(document).ready(function myFunction () {
+  $(".button").mouseenter(function(){
+    this.classList.add('button-testing');
+  });
+
+  $(".button").mouseleave(function(){
+    this.classList.remove('button-testing');
+  });
+
+  $(".button").mousedown(function(){
+    if(this.classList.contains("n1") && _n1Active){
+      for (const box of collectionN1) {
+        box.classList.remove('desc-project-opened');
+      }
+      //collectionN1.classList.remove('desc-project-opened');
+      return;
+    }
+    else if(this.classList.contains("n2") && _n2Active){
+      for (const box of collectionN2) {
+        box.classList.remove('desc-project-opened');
+      }
+      return;
+    }
+    
+    if (this.classList.contains("n1")){
+      _n1Active = true;
+      _n2Active = false;
+      for (const box of collectionN1) {
+        box.classList.add('desc-project-opened');
+      }
+      for (const box of collectionN2) {
+        box.classList.remove('desc-project-opened');
+      }
+    }
+    else{
+      _n1Active = false;
+      _n2Active = true;
+      for (const box of collectionN2) {
+        box.classList.add('desc-project-opened');
+      }
+      for (const box of collectionN1) {
+        box.classList.remove('desc-project-opened');
+      }
+    }
+  });
+});
